6d67e4c2af3d894c8a7590b403767a9d6816978f
Justin Willis <jgw9617@gmail.com>
2021-12-29 10:31:35 -0800

    perf(): webp

--- workbox.mjs
+++ workbox.mjs
@@ -3,7 +3,7 @@ import { injectManifest } from 'workbox-build';
 injectManifest({
     globDirectory: 'dist',
     globPatterns: [
-      '**/*.{html,json,js,css,png,webp,jpg}',
+      '**/*.{html,json,js,css}',
     ],
     swSrc: 'dist/pwabuilder-sw.js',
     swDest: 'dist/pwabuilder-sw.js',
